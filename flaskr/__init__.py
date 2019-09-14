5e1872448cc9c42c643f9b98194c5fbd1b3cb8ff
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2019-09-14 01:19:02 -0300

    Age and marital stacked bar chart

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 from flask import Flask, send_from_directory, render_template
 
@@ -8,10 +9,18 @@ def page_not_found(e):
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.register_error_handler(404, page_not_found)
+
+    # preparig data
+    df = pd.read_csv('data/bank-additional.csv', sep=";")
+    df['conversion'] = df['y'].apply(lambda x: 1 if x == 'yes' else 0)
     
     @app.route('/')
     def index():
-        return render_template('index.html')
+        model = {}
+        model['total_clients'] = df.shape[0]
+        model['total_conversions'] = df.conversion.sum()
+        model['conversion_rate'] = (model['total_conversions'] / model['total_clients']) * 100
+        return render_template('index.html', model=model)
 
     @app.route('/<path:path>')
     def send(path):
---
+++ flaskr/static/js/demo/chart-age-marital.js
@@ -0,0 +1,118 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+  // *     example: number_format(1234.56, 2, ',', ' ');
+  // *     return: '1 234,56'
+  number = (number + '').replace(',', '').replace(' ', '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
+// Bar Chart Example
+var ctx = document.getElementById("chartAgeMarital");
+var myBarChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["[18,30)", "[30,40)", "[40,50)", "[50,60)", "[60,70)", "70+"],
+    datasets: [{
+      label: "Divorced",
+      backgroundColor: "#4e73df",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#4e73df",
+      data: [0.21, 0.75, 1.19, 1.73, 3.72, 13.64]
+    },
+    {
+      label: "Married",
+      backgroundColor: "#73df4e",
+      hoverBackgroundColor: "#59d92e",
+      borderColor: "#4e73df",
+      data: [2.78, 5.29, 5.46, 7.76, 30.11, 32.19]
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        },
+        maxBarThickness: 25,
+        stacked: true
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 50.0,
+          maxTicksLimit: 10,
+          padding: 10,
+          callback: function(value, index, values) {
+            return number_format(value) + '%';
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': ' + number_format(tooltipItem.yLabel, 2, '.') + ' %';
+        }
+      }
+    },
+  }
+});
